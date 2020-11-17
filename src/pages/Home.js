632f5bc906ee7bb7377294cab94672b8567c9590
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-11-17 10:57:14 -0300

    banner component finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
  const Home = () => {
     return (
-    <Hero/>
+    <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms avaliable">
+            <Link to='/rooms' className="btn-primary">
+                Our Rooms
+            </Link>
+        </Banner>
+    </Hero>
     )
 }
 export default Home
