236042ce4de9eb998f25b6baa46747b01714b4dc
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-11-17 11:26:00 -0300

    Services component finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,19 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import Services from '../components/Services'
  const Home = () => {
     return (
-    <Hero>
-        <Banner title="luxurious rooms" subtitle="deluxe rooms avaliable">
-            <Link to='/rooms' className="btn-primary">
-                Our Rooms
-            </Link>
-        </Banner>
-    </Hero>
+    <>
+        <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms avaliable">
+                <Link to='/rooms' className="btn-primary">
+                    Our Rooms
+                </Link>
+            </Banner>
+        </Hero>
+        <Services/>
+    </>
     )
 }
 export default Home
